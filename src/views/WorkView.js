c48b155c351694ee8afaa0967dffb86e14b63b73
Rod Calumpong <Rod.Calumpong@gmail.com>
2014-06-13 21:00:33 -0700

    Implement arrow key movement on clicked squares

--- src/views/WorkView.js
+++ src/views/WorkView.js
@@ -9,7 +9,7 @@ define(function(require, exports, module) {
         View.apply(this, arguments);
         this.squares = {};
         this.numSquares = 0;
-        
+
         _createBackground.call(this);
     }
 
@@ -18,23 +18,26 @@ define(function(require, exports, module) {
     WorkView.prototype.createSquare = function() {
       this.numSquares++;
       console.log('creating square', this.numSquares);
-      
+
       var draggable = new Draggable();
-      
+
       var square = new Surface({
           size: [this.options.squareSize, this.options.squareSize],
           properties: {
               backgroundColor: 'pink',
           }
       });
-        
+
+
       var squareModifier = new StateModifier({
           size: [this.options.squareSize, this.options.squareSize],
           opacity: 1,
           origin: [0.5, 0.5],
           align: [0.5, 0.5]
       });
-      
+
+      _enableKeymovement(square, squareModifier);
+
       draggable.subscribe(square);
       var node = this.add(squareModifier).add(draggable).add(square);
       this.squares['square'+this.numSquares] = node;
@@ -43,7 +46,7 @@ define(function(require, exports, module) {
     WorkView.DEFAULT_OPTIONS = {
         squareSize: 50
     };
-    
+
     function _createBackground() {
         var background = new Surface({
             size: [undefined, undefined],
@@ -52,13 +55,67 @@ define(function(require, exports, module) {
                 zIndex: -1
             }
         });
-        
+
         var backgroundModifier = new StateModifier({
             transform: Transform.translate(0, 0, -1)
         });
-        
+
         this.add(backgroundModifier).add(background);
     }
 
     module.exports = WorkView;
+    var moveElement;
+
+    function _enableKeymovement(currentSurf, currentMod) {
+        var keyListener = function(currentMod) {
+            var x = currentMod._transformState._final[12];
+            var y = currentMod._transformState._final[13];
+
+            var moveElement = function(event) {
+                if (event.keyIdentifier === "Right") {
+                    if (event.shiftKey) {
+                    x = x + 5;
+                    } else {
+                        x++;
+                    }
+                    currentMod.setTransform(Transform.translate(x, y, 0));
+                } else if (event.keyIdentifier === "Down") {
+                    if (event.shiftKey) {
+                        y = y + 5;
+                    } else {
+                        y++;
+                    }
+                    currentMod.setTransform(Transform.translate(x, y, 0));
+                } else if (event.keyIdentifier === "Left") {
+                    if (event.shiftKey) {
+                        x = x - 5;
+                    } else {
+                        x--;
+                    }
+                    currentMod.setTransform(Transform.translate(x, y, 0));
+                } else if (event.keyIdentifier === "Up") {
+                    if (event.shiftKey) {
+                        y = y - 5;
+                    } else {
+                        y--;
+                    }
+                    currentMod.setTransform(Transform.translate(x, y, 0));
+                }
+            };
+
+            window.onkeydown = function(event) {
+                moveElement(event);
+            };
+        };
+
+        var keySilencer = function() {
+            window.removeEventListener('onkeydown', moveElement);
+        };
+
+        currentSurf.on('click', function(event) {
+            keySilencer();
+            keyListener(currentMod);
+        });
+
+    }
 });
