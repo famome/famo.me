170aa6e1d67db82c15a79c13aaf049a6687666f6
Aaron Ward <aaromp@gmail.com>
2014-06-16 14:08:07 -0700

    creates header and footer toggle tools

--- src/views/MenuView.js
+++ src/views/MenuView.js
@@ -15,7 +15,9 @@ define(function(require, exports, module) {
         var draggable = new Draggable();
 
         _createToolMenu.call(this, draggable);
-        _createSquareTool.call(this, draggable);
+        // _createSquareTool.call(this, draggable);
+        // _createHeaderTool.call(this, draggable);
+        _createFooterTool.call(this, draggable);
     }
 
     MenuView.prototype = Object.create(View.prototype);
@@ -56,6 +58,50 @@ define(function(require, exports, module) {
         
         draggable.subscribe(this.squareToolView);
         this.add(this.squareToolViewModifier).add(draggable).add(this.squareToolView);
+    }
+    
+    function _createHeaderTool(draggable) {
+        this.headerToolView = new ToolView();
+        this.headerToolViewModifier = new StateModifier({
+            transform: Transform.translate(0, this.options.topOffset, 1),
+            origin: [0.5, 0],
+            align: [0.5, 0]
+        });
+        
+        this.headerToolView.tool.setOptions({
+            content: '⬒',
+            origin: [0.5, 0.5],
+            align: [0.5, 0.5],
+            properties: {
+                textAlign: 'center',
+                fontSize: 40 + 'px'
+            }
+        });
+        
+        draggable.subscribe(this.headerToolView);
+        this.add(this.headerToolViewModifier).add(draggable).add(this.headerToolView);
+    }
+    
+    function _createFooterTool(draggable) {
+        this.footerToolView = new ToolView();
+        this.footerToolViewModifier = new StateModifier({
+            transform: Transform.translate(0, this.options.topOffset, 1),
+            origin: [0.5, 0],
+            align: [0.5, 0]
+        });
+        
+        this.footerToolView.tool.setOptions({
+            content: '⬓',
+            origin: [0.5, 0.5],
+            align: [0.5, 0.5],
+            properties: {
+                textAlign: 'center',
+                fontSize: 40 + 'px'
+            }
+        });
+        
+        draggable.subscribe(this.footerToolView);
+        this.add(this.footerToolViewModifier).add(draggable).add(this.footerToolView);
     }
 
     module.exports = MenuView;
--- src/views/WorkView.js
+++ src/views/WorkView.js
@@ -4,17 +4,43 @@ define(function(require, exports, module) {
     var Transform     = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
     var Draginator    = require('Draginator');
+    
+    var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+    var RenderController = require('famous/views/RenderController');
 
     function WorkView() {
         View.apply(this, arguments);
         this.squares = {};
         this.numSquares = 0;
         
-        _createBackground.call(this);
+        this.renderController = new RenderController();
+        this.add(this.renderController);
+        
+        _createContent.call(this);
     }
 
     WorkView.prototype = Object.create(View.prototype);
     WorkView.prototype.constructor = WorkView;
+    WorkView.prototype.toggleHeader = function() {
+        this.header = !this.header;
+        this.createHeaderFooter();
+    };
+    
+    WorkView.prototype.toggleFooter = function() {
+        this.footer = !this.footer;
+        this.createHeaderFooter();
+    };
+    
+    WorkView.prototype.createHeaderFooter = function() {
+        this.layout = new HeaderFooterLayout();
+        if (this.header) _createHeader.call(this);
+        
+        if (this.footer) _createFooter.call(this);
+        
+        if (this.header || this.footer) this.renderController.show(this.layout);
+        else this.renderController.hide(this.layout);
+    };
+    
     WorkView.prototype.createSquare = function() {
       this.numSquares++;
       console.log('creating square', this.numSquares);
@@ -49,11 +75,13 @@ define(function(require, exports, module) {
         squareSize: 50
     };
     
-    function _createBackground() {
+    function _createContent() {
         var background = new Surface({
             size: [undefined, undefined],
             classes: ["grey-bg"],
             properties: {
+                lineHeight: '150px',
+                textAlign: "center",
                 zIndex: -1
             }
         });
@@ -63,6 +91,46 @@ define(function(require, exports, module) {
         });
         
         this.add(backgroundModifier).add(background);
+    }
+    
+    function _createHeader() {
+        var header = new Surface({
+            size: [undefined, 100],
+            content: "Header",
+            classes: ["red-bg"],
+            properties: {
+                lineHeight: '100px',
+                textAlign: "center",
+                zIndex: -1
+            }
+        });
+        
+        // var headerModifier = new StateModifier({
+        //     transform: Transform.translate(0, 0, -1)
+        // });
+        
+        this.layout.header.add(header);
+        
+        // this.add(this.headerNode);
+    }
+    
+    function _createFooter() {
+        var footer = new Surface({
+            size: [undefined, 50],
+            content: "Footer",
+            classes: ["red-bg"],
+            properties: {
+                lineHeight: '50px',
+                textAlign: "center",
+                zIndex: -1
+            }
+        });
+        
+        // var footerModifier = new StateModifier({
+        //     transform: Transform.translate(0, 0, -1)
+        // });
+        
+        this.layout.footer.add(footer);        
     }
 
     module.exports = WorkView;
