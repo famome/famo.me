695cba9258bf55b523c917bf007adb6d75805e2c
Kayvon Ghashghai <kayvonghashghai@gmail.com>
2014-06-17 14:14:48 -0700

    merging

--- src/views/LayoutElementView.js
+++ src/views/LayoutElementView.js
@@ -6,12 +6,28 @@ define(function(require, exports, module) {
 
     function LayoutElementView() {
         View.apply(this, arguments);
+
+        var surface = new Surface();
+        surface.properties = {
+            backgroundColor: 'pink'
+        }
+
+        this.surface = surface;
+
+
+        window.le = this;
+        this.add(surface);
+        requestAnimationFrame(function() { surface._currTarget.onmousemove = _onMouseMove.bind(this) });
     }
 
     LayoutElementView.prototype = Object.create(View.prototype);
     LayoutElementView.prototype.constructor = LayoutElementView;
 
     LayoutElementView.DEFAULT_OPTIONS = {};
+
+    function _onMouseMove (event) {
+        console.log(event.target);
+    };
 
     module.exports = LayoutElementView;
 });
