beab9e197c55eeae04512b1d60adfa1e7d2f8f53
Kayvon Ghashghai <kayvonghashghai@gmail.com>
2014-06-17 14:14:19 -0700

    merging

--- src/views/MenuView.js
+++ src/views/MenuView.js
@@ -1,76 +1,108 @@
 define(function(require, exports, module) {
-    var Surface            = require('famous/core/Surface');
-    var Transform          = require('famous/core/Transform');
-    var StateModifier      = require('famous/modifiers/StateModifier');
-    var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+    var View          = require('famous/core/View');
+    var Surface       = require('famous/core/Surface');
+    var Transform     = require('famous/core/Transform');
+    var StateModifier = require('famous/modifiers/StateModifier');
+    var ModifierChain = require('famous/modifiers/ModifierChain');
+    var GridLayout    = require('famous/views/GridLayout');
+    var RenderNode    = require('famous/core/RenderNode');
+    var Draggable     = require('famous/modifiers/Draggable');
 
+    var ToolView      = require('views/ToolView');
 
-    var BasicLayout = {
-        createContent: function() {
-            var background = new Surface({
-                size: [undefined, undefined],
-                classes: ["grey-bg"],
-                properties: {
-                    lineHeight: '150px',
-                    textAlign: "center",
-                    zIndex: -1
-                }
-            });
+    function MenuView() {
+        View.apply(this, arguments);
 
-            var backgroundModifier = new StateModifier({
-                transform: Transform.translate(0, 0, -1)
-            });
+        var draggable = new Draggable();
 
-            this.add(backgroundModifier).add(background);
-        },
+        _createToolMenu.call(this, draggable);
+_createButtons.call(this, draggable);
+    }
 
-        createHeader: function() {
-            var header = new Surface({
-                size: [undefined, 100],
-                content: "Header",
-                classes: ["red-bg"],
-                properties: {
-                    lineHeight: '100px',
-                    textAlign: "center",
-                    zIndex: -1
-                }
-            });
+    MenuView.prototype = Object.create(View.prototype);
+    MenuView.prototype.constructor = MenuView;
 
-            var headerModifier = new StateModifier({
-                transform: Transform.translate(0, 0, -1)
-            });
-
-            this.layout.header.add(headerModifier).add(header);
-        },
-
-        createFooter: function() {
-            var footer = new Surface({
-                size: [undefined, 50],
-                content: "Footer",
-                classes: ["red-bg"],
-                properties: {
-                    lineHeight: '50px',
-                    textAlign: "center",
-                    zIndex: -1
-                }
-            });
-
-            var footerModifier = new StateModifier({
-                transform: Transform.translate(0, 0, -1)
-            });
-
-            this.layout.footer.add(footerModifier).add(footer);
-        },
-
-        render: function() {
-            this.layout = new HeaderFooterLayout();
-            if (this.header) BasicLayout.createHeader.call(this);
-            if (this.footer) BasicLayout.createFooter.call(this);
-
-            if (this.header || this.footer) this.renderController.show(this.layout);
-            else this.renderController.hide(this.layout);
-        }
+    MenuView.DEFAULT_OPTIONS = {
+        topOffset: 50
     };
 
-    module.exports = BasicLayout;
+    function _createToolMenu(draggable) {
+        this.toolMenu = new Surface({
+            size: [this.options.menuSize, undefined],
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            properties: {
+                backgroundColor: '#FFFFF5',
+                zIndex: 1,
+                boxShadow: "5px 5px 15px rgba(50, 50, 50, .5)",
+                height: this.options.topOffset
+            }
+        });
+
+        this.toolMenuModifier = new StateModifier({
+            opacity: 0.25,
+            transform: Transform.translate(0, 0, 1)
+        });
+
+        draggable.subscribe(this.toolMenu);
+
+        this.add(this.toolMenuModifier).add(draggable).add(this.toolMenu);
+    }
+
+    function _createButtons(draggable) {
+        var grid = new GridLayout({
+            dimensions: [2, 2],
+            gutterSize: [5, 5]
+    
+        });
+
+        var tools = [];
+        grid.sequenceFrom(tools);
+        var icons = ['⬒', '⬓', '⿳', '⿲'];
+
+        for (var i = 0; i < 4; i++) {
+            var toolView = new ToolView();
+            toolView.tool.setOptions({
+                content: icons[i],
+                size: [undefined, undefined],
+                origin: [0.5, 0.5],
+                align: [0.5, 0.5],
+                properties: {
+                    backgroundColor: 'pink',
+                    lineHeight: '60px',
+                    textAlign: 'center',
+                    fontSize: 40 + 'px',
+                    cursor: 'pointer'
+                }
+            });
+
+            toolView.tool.menu = this;
+
+            toolView.tool.on('click', function() {
+                this.menu.current = this.content;
+                this.menu._eventOutput.emit('menu');
+            });
+
+            tools.push(toolView.tool);
+        }
+
+
+        var gridModifier = new StateModifier({
+            size: [125, 125],
+            origin: [.5, .25],
+            transform: Transform.translate(0, 0, 1),
+            properties: {
+                zIndex: 1
+            }
+        });
+
+        draggable.subscribe(grid);
+
+        this.add(draggable).add(gridModifier).add(grid);
+        
+        draggable.subscribe(this.squareToolView);
+        this.add(this.squareToolViewModifier).add(draggable).add(this.squareToolView);
+    }
+
+    module.exports = MenuView;
 });
